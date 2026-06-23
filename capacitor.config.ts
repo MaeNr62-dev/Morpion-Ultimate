b023f9b2c8c66af6160479f227969e8ce97ae01e
Mae.Nr62 <150275442+MaeNr62-dev@users.noreply.github.com>
2026-06-23 23:14:33 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'fr.maenourry.morpionultimate.app',
-  appName: 'morpion-ultimate',
-  webDir: 'morpion-ultimate'
-};
-
-export default config;
